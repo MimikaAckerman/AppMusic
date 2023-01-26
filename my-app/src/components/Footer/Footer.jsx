b846ebe1eb2b2b988413d52da5898401007a0a0b
Mimika Ackerman <emily.herreragomez@gmail.com>
2023-01-26 14:12:54 +0100

    corrigiendo

--- my-app/src/components/Footer/Footer.jsx
+++ my-app/src/components/Footer/Footer.jsx
@@ -18,6 +18,7 @@ import { useForm } from "react-hook-form";
 //function create playlist
 import { addNewPlaylist } from "../../utils/addNewPlaylist";
 import { useAddPlaylistContext } from "../../context/AddPlaylistContext";
+import styled from "styled-components";
 
 const style = {
   position: "absolute",
@@ -30,6 +31,46 @@ const style = {
   boxShadow: 24,
   p: 4,
 };
+
+
+const HomeButton = styled.button`
+--glow-color: rgb(217, 176, 255);
+ --glow-spread-color: rgba(191, 123, 255, 0.781);
+ --enhanced-glow-color: rgb(231, 206, 255);
+ --btn-color: rgb(100, 61, 136);
+ border: .25em solid var(--glow-color);
+ padding: 1em 3em;
+ color: var(--glow-color);
+ font-size: 15px;
+ font-weight: bold;
+ background-color: var(--btn-color);
+ border-radius: 1em;
+ outline: none;
+ box-shadow: 0 0 1em .25em var(--glow-color),
+        0 0 4em 1em var(--glow-spread-color),
+        inset 0 0 .75em .25em var(--glow-color);
+ text-shadow: 0 0 .5em var(--glow-color);
+ position: relative;
+ transition: all 0.3s; 
+
+ HomeButton::after{
+  pointer-events: none;
+ content: "";
+ position: absolute;
+ top: 120%;
+ left: 0;
+ height: 100%;
+ width: 100%;
+ background-color: var(--glow-spread-color);
+ filter: blur(2em);
+ opacity: .7;
+ transform: perspective(1.5em) rotateX(35deg) scale(1, .6);
+ }
+`
+
+
+
+
 
 export default function Footer() {
   const [value, setValue] = React.useState(0);
@@ -56,6 +97,14 @@ const {playlist,setPlaylist} =useAddPlaylistContext()
   };
 
   return (
+    <>
+    <HomeButton>🏠</HomeButton>
+
+
+
+
+
+
     <Box sx={{ width: 427 }}>
       <BottomNavigation
         showLabels
@@ -130,5 +179,6 @@ const {playlist,setPlaylist} =useAddPlaylistContext()
         </Link>
       </BottomNavigation>
     </Box>
+    </>
   );
 }
