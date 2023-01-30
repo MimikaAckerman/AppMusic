7798e94cc75ab1fb27048ea1f97acd9ed683d843
Mimika Ackerman <emily.herreragomez@gmail.com>
2023-01-30 11:04:26 +0100

    corregir loginPage

--- my-app/src/pages/LoginPage/LoginPage.jsx
+++ my-app/src/pages/LoginPage/LoginPage.jsx
@@ -2,27 +2,17 @@ import React from "react";
 import styled from "styled-components";
 import cover from "../../assets/img/background.mp4";
 
-
 import { LoginButton } from "../../components/Form/Login/Login";
-import "./LoginPage.css";
 
 const LoginPage = () => {
-
   return (
     <>
-      
-
-      <video autoPlay loop muted playsInline className="background-video">
+      <BackgroundVideo autoPlay loop muted playsInline>
         <source src={cover} type="video/mp4" />
-       
-
-
-      </video>
+      </BackgroundVideo>
       <ButtonLogin>
-      
-      <LoginButton />
-    
-  </ButtonLogin>
+        <LoginButton />
+      </ButtonLogin>
     </>
   );
 };
@@ -30,10 +20,16 @@ const LoginPage = () => {
 export default LoginPage;
 
 const ButtonLogin = styled.div`
-position: absolute ;
-margin-top: 40rem ;
-margin-left:10rem ;
+  position: absolute;
+  margin-top: 40rem;
+  margin-left: 10rem;
+`;
 
-
-
-`
+const BackgroundVideo = styled.video`
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  min-width: 100%;
+  min-height: 100%;
+  width: 45rem;
+`;
