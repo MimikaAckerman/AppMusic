2694b584d5168294389f21bcff7a6598bd5163b2
Ziwen Wang <ziwen544newiz@gmail.com>
2023-02-03 02:41:17 +0100

    finalupdating beta

--- my-app/src/components/TracksContainer/TracksContainer.jsx
+++ my-app/src/components/TracksContainer/TracksContainer.jsx
@@ -11,6 +11,8 @@ import "swiper/css/pagination";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 import styled,{createGlobalStyle} from "styled-components";
+import { LogoutButton } from "../Form/Logout/Logout";
+import { Link } from "react-router-dom";
 
 const TracksContainer = () => {
   const { tracks } = useFetchApi();
@@ -54,8 +56,24 @@ const TracksContainer = () => {
   
         <GlobalStyle/>
        <DivTracks>
-
           <div className="containerTrack">
+       <BottonBack>
+          <Link to="/homePage">
+            <svg
+              stroke="currentColor"
+              fill="currentColor"
+              strokeWidth="0"
+              version="1.2"
+              baseProfile="tiny"
+              viewBox="0 0 24 24"
+              height="2em"
+              width="2em"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M12 9.059v-2.559c0-.256-.098-.512-.293-.708-.195-.195-.451-.292-.707-.292s-.512.097-.707.292l-6.293 6.208 6.293 6.207c.195.195.451.293.707.293s.512-.098.707-.293.293-.452.293-.707v-2.489c2.75.068 5.755.566 8 3.989v-1c0-4.633-3.5-8.443-8-8.941z"></path>
+            </svg>
+          </Link>
+        </BottonBack>
             <h1>nametrack</h1>
             <p className="nameTracks">{name}</p>
             <h2>trackartist</h2>
@@ -63,6 +81,7 @@ const TracksContainer = () => {
             <h2>genretrack</h2>
             <p className="genreTrack">{genre}</p>
             
+          <DivAudio>
 
             <AudioPlayer
               // autoPlay
@@ -74,9 +93,8 @@ const TracksContainer = () => {
               onClickNext={handleClickNext}
               onClickPrevious={handleClickPrevious}
               onEnded={handleEnd}
-            />
-
-
+              />
+              </DivAudio>
           </div>
       </DivTracks>
 
@@ -102,10 +120,35 @@ body{
 const DivTracks = styled.div`
   background: #efe2d5;
   width: 400px;
-  height: 640px;
+  height: 6780px;
   position: fixed;
   display: grid;
   text-align: center;
   margin-left: none;
-  margin-top: 35%;
+  margin-top: none;
+`
+
+const BottonBack = styled.button`
+  margin-top: 1rem;
+  margin-left: -70%;
+  text-align: left;
+  color: #090909;
+  padding: 0.2em 1.2em;
+  font-size: 18px;
+  border-radius: 0.5em;
+  background: #e8e8e8;
+  border: 1px solid #e8e8e8;
+  transition: all 0.3s;
+  box-shadow: 6px 6px 12px #c5c5c5, -6px -6px 12px #ffffff;
+  &:hover {
+    border: 1px solid white;
+  }
+  &:active {
+    box-shadow: 4px 4px 12px #c5c5c5, -4px -4px 12px #ffffff;
+  }
+`;
+
+const DivAudio = styled.div`
+  padding: 10px;
+  margin-left: -10px;
 `
